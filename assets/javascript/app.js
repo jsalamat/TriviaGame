dbd8235a2684c8a14339ec4fe3be4e6243ed829b
Jon Salamat <Jon Salamat>
2016-12-07 19:06:47 -0500

    questions arrange using objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,5 +16,41 @@
 // On the final screen, show the number of correct answers, incorrect answers,
 // and an option to restart the game (without reloading the page).
 
-alert('HI!');
+var timer = 30;
+var correctanswer = 0;
+var wronganswer = 0;
+var unanswered = 0;
 
+var questions = [
+	["What is questionA?", "option A1", "option A2", "optionA3", "option A4"],
+	["What is questionB?", "optionB1", "optionB2", "optionB3", "optionB4"],
+	["What is questionC?", "optionC1", "optionC2", "optionC3", "optionC4"],
+];
+
+var images = ["images/sample1.png", "images/sample2.png"]
+
+var showImage;
+
+	function quest1(){
+		$('#questions').html(questions[0][0]);
+
+		for (i = 1; i < questions[0].length; i++)
+			$('#choices').append('<div>' + questions[0][i] +'</div>');
+			$('#choices').on("click", function(k){
+				if ( k === "option A4"){
+					console.log('Winner')
+				}else{
+					console.log('Loser')
+				}
+			})
+
+	};
+
+	quest1();
+
+
+// one question at a time and with 30 sec timer
+//when click on answer it tell wether correct or not
+// it will give correct answer show for 4 sec and mov on to next question
+// timer goes down when hit zero show out of time and correct answer
+// at the end total of correct, wrong, and unanswered with start over button that reset the game
